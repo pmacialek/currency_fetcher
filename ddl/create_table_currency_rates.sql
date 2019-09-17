bef48266b985ba4cb5e0463d61c8c70c7b3a354c
pmacialek <przemek.macialek@pagisoft.pl>
2019-09-17 19:31:38 +0200

    Update create_table_currency_rates.sql

--- ddl/create_table_currency_rates.sql
+++ ddl/create_table_currency_rates.sql
@@ -1,9 +1,9 @@
 /* NOTE: change schema name */
 
-CREATE TABLE IF NOT EXISTS gimphoto_test_przemek.currency_rates (
-	currencyId INT AUTO_INCREMENT,
-	exchangeRateDate DATE,
-	currencyCode VARCHAR(20) NOT NULL,
-	currencyRate FLOAT,
-	PRIMARY KEY (currencyId)
+CREATE TABLE IF NOT EXISTS `gimphoto_test_przemek`.`currency_rates` (
+	`currencyId` INT AUTO_INCREMENT,
+	`exchangeRateDate` DATE,
+	`currencyCode` VARCHAR(20) NOT NULL,
+	`currencyRate` FLOAT,
+	PRIMARY KEY (`currencyId`)
 );
